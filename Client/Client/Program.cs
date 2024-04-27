a97bed69f8723cf7a3374d3cbbc72f0b24f2744c
Misteri <136891190+TkachevskiyOleg@users.noreply.github.com>
2024-04-27 09:18:37 +0300

    Add files via upload

---
+++ Client/Client/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Client
+{
+    static void Main(string[] args)
+    {
+        StartClient();
+    }
+
+    static void StartClient()
+    {
+        string serverIp = "127.0.0.1";
+        int port = 12345;
+
+        TcpClient client = new TcpClient();
+
+        try
+        {
+            client.Connect(serverIp, port);
+            Console.WriteLine("Connected to the server...");
+
+            string message = "Hello, server!";
+            byte[] data = Encoding.UTF8.GetBytes(message);
+            NetworkStream stream = client.GetStream();
+            stream.Write(data, 0, data.Length);
+            Console.WriteLine($"At {DateTime.Now:t} sent string: {message}");
+
+            byte[] responseData = new byte[256];
+            int bytesRead = stream.Read(responseData, 0, responseData.Length);
+            string responseMessage = Encoding.UTF8.GetString(responseData, 0, bytesRead);
+            Console.WriteLine($"At {DateTime.Now:t} received string: {responseMessage}");
+
+            client.Close();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+}
---
+++ Server/Server/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Server
+{
+    static void Main(string[] args)
+    {
+        StartServer();
+    }
+
+    static void StartServer()
+    {
+        IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+        int port = 12345;
+
+        TcpListener listener = new TcpListener(ipAddress, port);
+        listener.Start();
+        Console.WriteLine("Server started...");
+
+        while (true)
+        {
+            TcpClient client = listener.AcceptTcpClient();
+            Console.WriteLine("Client connected...");
+
+            NetworkStream stream = client.GetStream();
+            byte[] data = new byte[256];
+            int bytesRead = stream.Read(data, 0, data.Length);
+            string message = Encoding.UTF8.GetString(data, 0, bytesRead);
+            Console.WriteLine($"At {DateTime.Now:t} from {((IPEndPoint)client.Client.RemoteEndPoint).Address} received string: {message}");
+
+            string responseMessage = "Hello, client!";
+            byte[] responseData = Encoding.UTF8.GetBytes(responseMessage);
+            stream.Write(responseData, 0, responseData.Length);
+            Console.WriteLine("Response sent to the client.");
+
+            client.Close();
+        }
+    }
+}
